9673fc09469ac646f41379a9989f70aad27d0f59
Abdurrahman <abdurrhmanfaid@gmail.com>
2020-10-03 23:16:37 +0200

    Be ready to play

--- public/js/app.js
+++ public/js/app.js
@@ -1,6 +1,26 @@
-var app = new Vue({
-    el: '#app',
-    data: {
-        message: 'Hello Vue!'
-    }
+const wHeight =  window.innerHeight
+const wWidth = window.innerWidth
+let canvas = document.querySelector('#the-canvas')
+let context = canvas.getContext('2d')
+canvas.width = wWidth
+canvas.height = wHeight
+let player = {}
+
+$(window).load(function() {
+    $('#loginModal').modal('show');
+});
+
+$('.name-form').submit(e => {
+    e.preventDefault()
+    player.name = document.querySelector('#name-input').value
+    if (! player.name) return;
+    $('#loginModal').modal('hide');
+    $('#spawnModal').modal('show');
+    document.querySelector('.player-name').innerHTML = 'Hello, ' + player.name
+})
+
+$('.start-game').click(e => {
+    $('.modal').modal('hide')
+    $('.hiddenOnStart').removeAttr('hidden')
+    init()
 })
